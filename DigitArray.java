874f548fae121b99a7eaf90328327ee5be3eab88
BionicPi <mxmeinhold@gmail.com>
2015-05-13 19:35:54 -0400

    Add method to initialize an ArrayList of   size   ints.

--- DigitArray.java
+++ DigitArray.java
@@ -1,4 +1,11 @@
 public class DigitArray
 {
-
+    public static <int> createDigitArr(List<int> digarr, int size)
+    {
+        for(int i = 0; i < size; i++)
+        {
+            digarr.add(new int(0));
+        }
+        return digarr;
+    }
 }
