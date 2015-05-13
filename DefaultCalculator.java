1a53f12a27500f93c32d29370b2a576f77eb3d02
BionicPi <mxmeinhold@gmail.com>
2015-05-13 18:47:28 -0400

    Create DefaultCalculator.java
    Basic first draft of the calculator.

---
+++ DefaultCalculator.java
@@ -0,0 +1,29 @@
+public class AndreCalculator
+{
+	/**
+	*@param first the first integer to be multiplied
+	*@param second the second integer to be multiplied
+	*/
+	public static void multiply(int first, int second)
+	{
+		
+	}
+	
+	/**
+	*@param first the first double to be multiplied
+	*@param second the second double to be multiplied
+	*/
+	public static void multiplu(double first, double second)
+	{
+		
+	}
+	
+	/**
+	*@param first the first number (stored as a string to bypass length limits) to be multiplied
+	*@param second the second number (stored as a string to bypass length limits) to be multiplied
+	*/
+	public static void multiply(String first, String second)
+	{
+	
+	}
+}
