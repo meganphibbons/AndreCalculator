aac8f42678ba0cbef20c8b3d42eb374b5216dfe6
BionicPi <mxmeinhold@gmail.com>
2015-05-14 16:58:54 -0400

    Add tester file

---
+++ Tester.java
@@ -0,0 +1,11 @@
+import DefaultCalculator;
+
+public class Tester
+{
+	public static void main(String[] args)
+	{
+		String first = "2000000000";
+		String second = "1853000000";
+		multiply(first, second);
+	}
+}
