50f87fba31a91e07187f57c2d4a80f87c521790c
BionicPi <mxmeinhold@gmail.com>
2015-05-16 11:27:36 -0400

    Begin working on decimal multiplication.
    Disable unused methods.

--- DefaultCalculator.java
+++ DefaultCalculator.java
@@ -1,3 +1,7 @@
+/**
+ * @author Max (BionicPi)
+ */
+
 package AndreCalculator;
 
 import java.util.ArrayList;
@@ -8,50 +12,55 @@ public class DefaultCalculator
 	private static List<Integer> digits = new ArrayList<Integer>();
 	private static List<Integer> digits2 = new ArrayList<Integer>();
 	private static List<Integer> results = new ArrayList<Integer>();
+	
+	private static List<Integer> digitsDecimal = new ArrayList<Integer>();
+	private static List<Integer> digits2Decimal = new ArrayList<Integer>();
+	private static List<Integer> resultsDecimal = new ArrayList<Integer>();
+				//UNUSED
+//	/**
+//	 *Multiplies two numbers stored as ints. Prints result as a string
+//	 *@param first the first integer to be multiplied
+//	 *@param second the second integer to be multiplied
+//	 */
+//	public static String multiply(int first, int second)
+//	{
+//		if(digits.size()>0)
+//		{
+//			digits = new ArrayList<Integer>();
+//			digits2 = new ArrayList<Integer>();
+//		}
+//
+//		int f = first;
+//		int s = second;
+//		while(f/10 >= 0)
+//		{
+//			digits.add(f%10);
+//			f /= 10;
+//		}
+//		while(s/10 >= 0)
+//		{
+//			digits.add(s%10);
+//			s /= 10;
+//		}
+//		//System.out.println(intMultiplication());
+//		return intMultiplication();
+//	}
+				//UNUSED
+//	/**
+//	 * Multiplies two numbers stored as doubles. Prints result as a string
+//	 *@param first the first double to be multiplied
+//	 *@param second the second double to be multiplied
+//	 */
+//	public static String multiply(double first, double second)
+//	{
+//		return null;
+//	}
 
 	/**
-	 *Multiplies two numbers stored as ints. Prints result as a string
-	 *@param first the first integer to be multiplied
-	 *@param second the second integer to be multiplied
-	 */
-	public static String multiply(int first, int second)
-	{
-		if(digits.size()>0)
-		{
-			digits = new ArrayList<Integer>();
-			digits2 = new ArrayList<Integer>();
-		}
-
-		int f = first;
-		int s = second;
-		while(f/10 >= 0)
-		{
-			digits.add(f%10);
-			f /= 10;
-		}
-		while(s/10 >= 0)
-		{
-			digits.add(s%10);
-			s /= 10;
-		}
-		//System.out.println(intMultiplication());
-		return intMultiplication();
-	}
-
-	/**
-	 * Multiplies two numbers stored as doubles. Prints result as a string
-	 *@param first the first double to be multiplied
-	 *@param second the second double to be multiplied
-	 */
-	public static String multiply(double first, double second)
-	{
-		return null;
-	}
-
-	/**
-	 * Multiplies two numbers stored as strings. Prints result as a string
+	 * Multiplies two numbers stored as strings. Returns result as a string
 	 *@param first the first number (stored as a string to bypass length limits) to be multiplied
 	 *@param second the second number (stored as a string to bypass length limits) to be multiplied
+	 *@return the product of the inputs, as a string
 	 */
 	public static String multiply(String first, String second)
 	{
@@ -94,6 +103,7 @@ public class DefaultCalculator
 				}
 			}
 		}
+		
 		for(int i =0; i < results.size(); i++)
 		{
 			if(results.get(i)>=10)
@@ -108,11 +118,60 @@ public class DefaultCalculator
 				results.set(i, results.get(i)%10);
 			}
 		}
+		
 		String number = "";
 		for(int num=results.size()-1; num>=0; num--)
 		{
 			number+=results.get(num);
 		}
 		return number;
+	}
+	
+	/**
+	 * Computes and returns the product of the numbers stored in the ArrayLists
+	 * @return the product of the numbers stored in the array lists as a String
+	 */
+	private static String decimalMultiplication()
+	{
+		for(int i=0; i< digits.size(); i++)
+		{
+			for(int j = 0; j<digits2.size(); j++)
+			{
+				try
+				{
+					results.set(i+j, results.get(i+j) + digits.get(i)*digits2.get(j));
+				}catch(IndexOutOfBoundsException e)
+				{
+					for(int c = results.size(); c<= i+j; c++)
+						results.add(0);
+					results.set(i+j, results.get(i+j) + digits.get(i)*digits2.get(j));
+				}
+			}
+		}
+		
+		//TODO track tenths place for decimal results, add secondry forloop to multiply the decimals.
+		
+		for(int i =0; i < results.size(); i++)
+		{
+			if(results.get(i)>=10)
+			{
+				try
+				{
+					results.set(i+1, results.get(i+1)+results.get(i)/10);
+				}catch(IndexOutOfBoundsException e)
+				{
+					results.add(results.get(i)/10);
+				}
+				results.set(i, results.get(i)%10);
+			}
+		}
+		
+//		String number = "";
+//		for(int num=results.size()-1; num>=0; num--)
+//		{
+//			number+=results.get(num);
+//		}
+//		return number;
+		return "This function isn't yet avalible.";
 	}
 }
