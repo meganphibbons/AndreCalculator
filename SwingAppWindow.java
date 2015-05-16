d2f191a19ecb1f4f25e607a738f0caec20fc5a70
BionicPi <mxmeinhold@gmail.com>
2015-05-16 09:21:43 -0400

    Adjust GUI.

--- SwingAppWindow.java
+++ SwingAppWindow.java
@@ -12,10 +12,11 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Font;
 
 public class SwingAppWindow {
 
-	private JFrame frame;
+	private JFrame frmLargeNumberMultiplier;
 	private JTextField first;
 	private JTextField second;
 	private JTextField textField;
@@ -28,7 +29,7 @@ public class SwingAppWindow {
 			public void run() {
 				try {
 					SwingAppWindow window = new SwingAppWindow();
-					window.frame.setVisible(true);
+					window.frmLargeNumberMultiplier.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -47,51 +48,44 @@ public class SwingAppWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 600, 220);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLargeNumberMultiplier = new JFrame();
+		frmLargeNumberMultiplier.setFont(new Font("Serpentine Std", Font.PLAIN, 12));
+		frmLargeNumberMultiplier.setTitle("Large Number Multiplier");
+		frmLargeNumberMultiplier.setBounds(100, 100, 620, 260);
+		frmLargeNumberMultiplier.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[] {200, 200, 200};
+		gridBagLayout.columnWidths = new int[] {10, 200, 200, 200, 10};
 		gridBagLayout.rowHeights = new int[] {60, 60, 40, 60};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0};
-		frame.getContentPane().setLayout(gridBagLayout);
+		frmLargeNumberMultiplier.getContentPane().setLayout(gridBagLayout);
 		
 		JLabel lblFirstNumber = new JLabel("First Number");
 		lblFirstNumber.setToolTipText("You're investigating...");
 		GridBagConstraints gbc_lblFirstNumber = new GridBagConstraints();
 		gbc_lblFirstNumber.insets = new Insets(0, 0, 5, 5);
 		gbc_lblFirstNumber.fill = GridBagConstraints.VERTICAL;
-		gbc_lblFirstNumber.gridx = 0;
+		gbc_lblFirstNumber.gridx = 1;
 		gbc_lblFirstNumber.gridy = 0;
-		frame.getContentPane().add(lblFirstNumber, gbc_lblFirstNumber);
+		frmLargeNumberMultiplier.getContentPane().add(lblFirstNumber, gbc_lblFirstNumber);
 		
 		JLabel lblSecondNumber = new JLabel("Second Number");
 		lblSecondNumber.setToolTipText("");
 		GridBagConstraints gbc_lblSecondNumber = new GridBagConstraints();
-		gbc_lblSecondNumber.insets = new Insets(0, 0, 5, 0);
-		gbc_lblSecondNumber.gridx = 2;
+		gbc_lblSecondNumber.insets = new Insets(0, 0, 5, 5);
+		gbc_lblSecondNumber.gridx = 3;
 		gbc_lblSecondNumber.gridy = 0;
-		frame.getContentPane().add(lblSecondNumber, gbc_lblSecondNumber);
+		frmLargeNumberMultiplier.getContentPane().add(lblSecondNumber, gbc_lblSecondNumber);
 		
 		first = new JTextField();
 		first.setToolTipText("");
 		GridBagConstraints gbc_first = new GridBagConstraints();
 		gbc_first.fill = GridBagConstraints.BOTH;
 		gbc_first.insets = new Insets(0, 0, 5, 5);
-		gbc_first.gridx = 0;
+		gbc_first.gridx = 1;
 		gbc_first.gridy = 1;
-		frame.getContentPane().add(first, gbc_first);
+		frmLargeNumberMultiplier.getContentPane().add(first, gbc_first);
 		first.setColumns(10);
-		
-		second = new JTextField();
-		GridBagConstraints gbc_second = new GridBagConstraints();
-		gbc_second.fill = GridBagConstraints.BOTH;
-		gbc_second.insets = new Insets(0, 0, 5, 0);
-		gbc_second.gridx = 2;
-		gbc_second.gridy = 1;
-		frame.getContentPane().add(second, gbc_second);
-		second.setColumns(10);
 		
 		JButton btnMultiply = new JButton("Multiply");
 		btnMultiply.addMouseListener(new MouseAdapter() {
@@ -100,22 +94,31 @@ public class SwingAppWindow {
 				textField.setText(DefaultCalculator.multiply(first.getText(), second.getText()));
 			}
 		});
+		
+		second = new JTextField();
+		GridBagConstraints gbc_second = new GridBagConstraints();
+		gbc_second.fill = GridBagConstraints.BOTH;
+		gbc_second.insets = new Insets(0, 0, 5, 5);
+		gbc_second.gridx = 3;
+		gbc_second.gridy = 1;
+		frmLargeNumberMultiplier.getContentPane().add(second, gbc_second);
+		second.setColumns(10);
 		btnMultiply.setToolTipText("GET ON WITH IT.");
 		GridBagConstraints gbc_btnMultiply = new GridBagConstraints();
 		gbc_btnMultiply.anchor = GridBagConstraints.SOUTH;
 		gbc_btnMultiply.insets = new Insets(0, 0, 5, 5);
-		gbc_btnMultiply.gridx = 1;
+		gbc_btnMultiply.gridx = 2;
 		gbc_btnMultiply.gridy = 2;
-		frame.getContentPane().add(btnMultiply, gbc_btnMultiply);
+		frmLargeNumberMultiplier.getContentPane().add(btnMultiply, gbc_btnMultiply);
 		
 		textField = new JTextField();
 		GridBagConstraints gbc_output = new GridBagConstraints();
+		gbc_output.insets = new Insets(0, 0, 0, 5);
 		gbc_output.fill = GridBagConstraints.HORIZONTAL;
 		gbc_output.gridwidth = 3;
-		gbc_output.insets = new Insets(0, 0, 0, 5);
-		gbc_output.gridx = 0;
+		gbc_output.gridx = 1;
 		gbc_output.gridy = 3;
-		frame.getContentPane().add(textField, gbc_output);
+		frmLargeNumberMultiplier.getContentPane().add(textField, gbc_output);
 		textField.setColumns(10);
 	}
 
