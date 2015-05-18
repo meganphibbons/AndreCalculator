8e58f0360e4259ba789400196849d340d68eb5e2
BionicPi <mxmeinhold@gmail.com>
2015-05-18 15:34:07 -0400

    Test new public method, exponent.

--- Tester.java
+++ Tester.java
@@ -8,5 +8,9 @@ public class Tester
 		String second = "20.02";
 		System.out.println(first + " times " + second);
 		DefaultCalculator.multiply(first, second);
+		
+		String base = "2";
+		String power = "1024";
+		System.out.println(DefaultCalculator.exponent(base, power));
 	}
 }
